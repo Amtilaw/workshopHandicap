95b1f2266f08469a89d143bee4de7b478575b2dc
Jeanjean <adrien.jeanjean@epsi.fr>
2021-10-06 12:59:46 +0200

    BACKEND page cours 🚀

--- src/Controller/CourController.php
+++ src/Controller/CourController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CourController extends AbstractController
 {
     /**
      * @Route("/cour", name="cour")
      */
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+      $i = $_GET["num"];
+      $cour = $connection->fetchAll("SELECT * FROM contenu WHERE id_contenu = ". $i);
+      
+
         return $this->render('cour/index.html.twig', [
             'controller_name' => 'CourController',
+            'cour' => $cour,
         ]);
     }
 }
