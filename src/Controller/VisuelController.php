7462416068a91af129ceca8a6aea1acda99209d5
Jeanjean <adrien.jeanjean@epsi.fr>
2021-10-07 11:54:07 +0200

    Page Visuel & dossier resource 📝

--- src/Controller/VisuelController.php
+++ src/Controller/VisuelController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Driver\Connection;
 
 class VisuelController extends AbstractController
 {
     /**
      * @Route("/visuel", name="visuel")
      */
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+
+      $imgUrl = $connection->fetchAll("SELECT visuel FROM methode");
+
         return $this->render('visuel/index.html.twig', [
             'controller_name' => 'VisuelController',
+            'img' => $imgUrl,
         ]);
     }
 }
