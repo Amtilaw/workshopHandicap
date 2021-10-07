12180918ec1ef18c7613bd9f51a3012733eac205
Jeanjean <adrien.jeanjean@epsi.fr>
2021-10-07 14:59:55 +0200

    modif 🚑

--- src/Controller/AuditifController.php
+++ src/Controller/AuditifController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,14 @@ class AuditifController extends AbstractController
     /**
      * @Route("/auditif", name="auditif")
      */
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+
+      $i = $_GET["num"];
+        $auditif = $connection->fetchAll("SELECT auditif FROM methode WHERE id_methode = ". $i);
         return $this->render('auditif/index.html.twig', [
             'controller_name' => 'AuditifController',
+            'auditif' => $auditif,
         ]);
     }
 }
